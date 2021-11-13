79c76cc686219282a59e5042593d30577aa19861
Kamrul-Haque <kamrul35-255@diu.edu.bd>
2021-11-13 21:38:13 +0600

    Backing Up

---
+++ Assets/Scripts/MosueJumper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// object jumps on mouse click
+/// </summary>
+public class MosueJumper : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Input.mousePosition;
+            pos.z = -Camera.main.transform.position.z;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            transform.position = pos;
+        }
+    }
+}
